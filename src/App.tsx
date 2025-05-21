729e5e59229b275b5fdaf261c0100daadc4b9e73
hoaiduong0802 <hongthai2007.hongthai2007@gmail.com>
2025-05-21 23:51:38 +0700

    Update | Build Structure Folder | HaitNguyen

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
 import '../public/scss/style.scss'
 function App() {
 
   return (
     // Demo component 
     <>
-      <section className='section-1'>
+      <section className='container section-1'>
         <div>
           <h1>Sản Phẩm Được Đề Xuất Cho Bạn</h1>
           <p>Khám phá sản phẩm được yêu thích và đánh giá cao nhất bởi khách hàng của chúng tôi.
