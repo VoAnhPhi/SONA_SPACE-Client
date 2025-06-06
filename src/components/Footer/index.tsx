c21b9dcfd35a9c506b18af6654e04728b60c2062
hoaiduong0802 <hongthai2007.hongthai2007@gmail.com>
2025-06-06 16:26:04 +0700

    Build | Home Page & Footer, Updated Box-shadow | HaitNguyen

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -6,57 +6,75 @@ const Footer = () => {
         <div className="container">
           <div className="footer-grid">
             <div className="footer-column">
-              <h3>Dịch vụ khách hàng</h3>
               <ul>
                 <li>
-                  <a href="/lien-he">Liên hệ</a>
+                  <a href="/dich-vu">Dịch vụ khách hàng</a>
                 </li>
                 <li>
-                  <a href="/tra-cuu-don-hang">Tra cứu đơn hàng</a>
+                  <a href="/cua-hang-gan-ban">Tìm cửa hàng gần bạn</a>
                 </li>
                 <li>
-                  <a href="/chinh-sach-van-chuyen">Chính sách vận chuyển</a>
+                  <a href="/the-qua-tang">Mua thẻ quà tặng</a>
                 </li>
                 <li>
-                  <a href="/chinh-sach-doi-tra">Chính sách đổi trả</a>
+                  <a href="/ve-chung-toi">Về Sona Space</a>
                 </li>
               </ul>
             </div>
             <div className="footer-column">
-              <h3>Thông tin</h3>
               <ul>
                 <li>
-                  <a href="/ve-chung-toi">Về chúng tôi</a>
+                  <a href="/room">Phong cách</a>
                 </li>
                 <li>
-                  <a href="/blog">Blog</a>
+                  <a href="/ho-so">Trung tâm hỗ trợ</a>
                 </li>
                 <li>
-                  <a href="/cua-hang">Cửa hàng</a>
+                  <a href="/ve-chung-toi">Câu chuyện thương hiệu</a>
                 </li>
               </ul>
             </div>
             <div className="footer-column">
-              <h3>Liên hệ với chúng tôi</h3>
               <ul>
                 <li>
-                  <a href="tel:+84123456789">+84 123 456 789</a>
+                  <a href="/faq">Câu hỏi thường gặp</a>
                 </li>
                 <li>
-                  <a href="mailto:info@sonaspace.com">info@sonaspace.com</a>
+                  <a href="/orders">Theo dõi đơn hàng</a>
                 </li>
                 <li>
-                  <p>123 Đường ABC, Quận XYZ, TP.HCM</p>
+                  <a href="/privacy-policy">Chính sách đổi trả</a>
+                </li>
+              </ul>
+            </div>
+            <div className="footer-column">
+              <ul>
+                <li>
+                  <a href="/thong-tin-van-chuyen">Thông tin vận chuyển</a>
+                </li>
+                <li>
+                  <a href="/khuyen-mai">Chương trình thành viên</a>
+                </li>
+                <li>
+                  <a href="/khuyen-mai-doc-quyen">Ưu đãi độc quyền</a>
                 </li>
               </ul>
             </div>
             <div className="footer-column">
               <h3>Theo dõi chúng tôi</h3>
               <div className="social-icons">
-                <a href="#" className="social-icon facebook"></a>
-                <a href="#" className="social-icon instagram"></a>
-                <a href="#" className="social-icon youtube"></a>
-                <a href="#" className="social-icon tiktok"></a>
+                <a href="#" className="social-icon facebook">
+                  <img src="./public/images/icons/facebook.svg" alt="" />
+                </a>
+                <a href="#" className="social-icon instagram">
+                  <img src="./public/images/icons/ins.svg" alt="" />
+                </a>
+                <a href="#" className="social-icon pinterest">
+                  <img src="./public/images/icons/pint.svg" alt="" />
+                </a>
+                <a href="#" className="social-icon phone">
+                  <img src="./public/images/icons/phone.svg" alt="" />
+                </a>
               </div>
             </div>
             <div className="footer-column">
@@ -72,8 +90,12 @@ const Footer = () => {
               Tất cả giá đều là giá bán lẻ đề xuất bằng Việt Nam Đồng (₫) và đã
               bao gồm VAT.
             </p>
-            <p><a href="/chinh-sach-bao-mat">Thông tin cookie</a></p>
-            <p><a href="/dieu-khoan-su-dung">Chính sách bảo mật</a></p>
+            <p>
+              <a href="/chinh-sach-bao-mat">Thông tin cookie</a>
+            </p>
+            <p>
+              <a href="/dieu-khoan-su-dung">Chính sách bảo mật</a>
+            </p>
             <div className="payment-icons">
               <img src="/images/payment-icons.png" alt="Payment methods" />
             </div>
