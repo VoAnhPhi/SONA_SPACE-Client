756bf5bfc1b20f0b1e2178f679968502fe320366
ToTrongNhan1209 <totrongnhan1209.com>
2025-06-08 03:49:55 +0700

    Build | Layout Payment | TrongNhan

--- src/pages/ProductOfCategory/index.tsx
+++ src/pages/ProductOfCategory/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
-import ListProduct from "../../components/ListProduct";
+import ListProduct from "../../components/listProduct";
 import Filter from "../../components/filter";
 
 interface ProductProps {
