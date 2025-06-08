1ed1ecfb28d6780bb2c4a318147d5b5d6c0329f8
hoaiduong0802 <hongthai2007.hongthai2007@gmail.com>
2025-06-08 20:32:36 +0700

    Updated | Responsive For All Devices | HaitNguyen

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,7 +4,6 @@ const Header = () => {
       {/* Header/Navigation */}
       <header className="header">
         <div className="container">
-
           <div className="header-wrapper">
             <div className="logo">
               <a href="/">
@@ -19,23 +18,55 @@ const Header = () => {
                   </a>
                 </li>
                 <li>
-                  <a href="/san-pham">  Sản Phẩm
+                  <a href="/san-pham">
+                    {" "}
+                    Sản Phẩm
                     <img src="../public/images/icons/arrow-down.svg" alt="" />
                     <div className="sub-menu">
                       <div className="container">
                         <div className="sub-menu-wrapper">
                           <div className="sub-menu-column">
                             <ul>
-                              <li><a href="/san-pham/sofa">Sofa</a></li>
-                              <li><a href="/san-pham/ban-lam-viec">Bàn làm việc</a></li>
-                              <li><a href="/san-pham/tab-dau-giuong">Tab đầu giường</a></li>
-                              <li><a href="/san-pham/tu-trang-tri">Tủ trang trí</a></li>
-                              <li><a href="/san-pham/tham">Thảm</a></li>
-                              <li><a href="/san-pham/ghe-don">Ghế đơn</a></li>
-                              <li><a href="/san-pham/den-ban-an">Đèn bàn ăn</a></li>
-                              <li><a href="/san-pham/ghe-van-phong">Ghế văn phòng</a></li>
-                              <li><a href="/san-pham/den-trang-tri">Đèn trang trí</a></li>
-                              <li><a href="/san-pham/ke-sach">Kệ sách</a></li>
+                              <li>
+                                <a href="/san-pham/sofa">Sofa</a>
+                              </li>
+                              <li>
+                                <a href="/san-pham/ban-lam-viec">
+                                  Bàn làm việc
+                                </a>
+                              </li>
+                              <li>
+                                <a href="/san-pham/tab-dau-giuong">
+                                  Tab đầu giường
+                                </a>
+                              </li>
+                              <li>
+                                <a href="/san-pham/tu-trang-tri">
+                                  Tủ trang trí
+                                </a>
+                              </li>
+                              <li>
+                                <a href="/san-pham/tham">Thảm</a>
+                              </li>
+                              <li>
+                                <a href="/san-pham/ghe-don">Ghế đơn</a>
+                              </li>
+                              <li>
+                                <a href="/san-pham/den-ban-an">Đèn bàn ăn</a>
+                              </li>
+                              <li>
+                                <a href="/san-pham/ghe-van-phong">
+                                  Ghế văn phòng
+                                </a>
+                              </li>
+                              <li>
+                                <a href="/san-pham/den-trang-tri">
+                                  Đèn trang trí
+                                </a>
+                              </li>
+                              <li>
+                                <a href="/san-pham/ke-sach">Kệ sách</a>
+                              </li>
                             </ul>
                           </div>
                           <div className="sub-menu-products">
@@ -75,31 +106,41 @@ const Header = () => {
                               <div className="product-item">
                                 <a href="/san-pham/tu-trang-tri">
                                   <div className="product-image"></div>
-                                  <div className="product-title">Tủ trang trí</div>
+                                  <div className="product-title">
+                                    Tủ trang trí
+                                  </div>
                                 </a>
                               </div>
                               <div className="product-item">
                                 <a href="/san-pham/den-phong-khach">
                                   <div className="product-image"></div>
-                                  <div className="product-title">Đèn phòng khách</div>
+                                  <div className="product-title">
+                                    Đèn phòng khách
+                                  </div>
                                 </a>
                               </div>
                               <div className="product-item">
                                 <a href="/san-pham/giuong-ngu">
                                   <div className="product-image"></div>
-                                  <div className="product-title">Giường ngủ</div>
+                                  <div className="product-title">
+                                    Giường ngủ
+                                  </div>
                                 </a>
                               </div>
                               <div className="product-item">
                                 <a href="/san-pham/tu-quan-ao">
                                   <div className="product-image"></div>
-                                  <div className="product-title">Tủ quần áo</div>
+                                  <div className="product-title">
+                                    Tủ quần áo
+                                  </div>
                                 </a>
                               </div>
                               <div className="product-item">
                                 <a href="/san-pham/ban-trang-diem">
                                   <div className="product-image"></div>
-                                  <div className="product-title">Bàn trang điểm</div>
+                                  <div className="product-title">
+                                    Bàn trang điểm
+                                  </div>
                                 </a>
                               </div>
                             </div>
@@ -110,23 +151,54 @@ const Header = () => {
                   </a>
                 </li>
                 <li>
-                  <a href="/khong-gian">Không Gian
+                  <a href="/khong-gian">
+                    Không Gian
                     <img src="../public/images/icons/arrow-down.svg" alt="" />
                     <div className="sub-menu">
                       <div className="container">
                         <div className="sub-menu-wrapper">
                           <div className="sub-menu-column">
                             <ul>
-                              <li><a href="/san-pham/sofa">Sofa</a></li>
-                              <li><a href="/san-pham/ban-lam-viec">Bàn làm việc</a></li>
-                              <li><a href="/san-pham/tab-dau-giuong">Tab đầu giường</a></li>
-                              <li><a href="/san-pham/tu-trang-tri">Tủ trang trí</a></li>
-                              <li><a href="/san-pham/tham">Thảm</a></li>
-                              <li><a href="/san-pham/ghe-don">Ghế đơn</a></li>
-                              <li><a href="/san-pham/den-ban-an">Đèn bàn ăn</a></li>
-                              <li><a href="/san-pham/ghe-van-phong">Ghế văn phòng</a></li>
-                              <li><a href="/san-pham/den-trang-tri">Đèn trang trí</a></li>
-                              <li><a href="/san-pham/ke-sach">Kệ sách</a></li>
+                              <li>
+                                <a href="/san-pham/sofa">Sofa</a>
+                              </li>
+                              <li>
+                                <a href="/san-pham/ban-lam-viec">
+                                  Bàn làm việc
+                                </a>
+                              </li>
+                              <li>
+                                <a href="/san-pham/tab-dau-giuong">
+                                  Tab đầu giường
+                                </a>
+                              </li>
+                              <li>
+                                <a href="/san-pham/tu-trang-tri">
+                                  Tủ trang trí
+                                </a>
+                              </li>
+                              <li>
+                                <a href="/san-pham/tham">Thảm</a>
+                              </li>
+                              <li>
+                                <a href="/san-pham/ghe-don">Ghế đơn</a>
+                              </li>
+                              <li>
+                                <a href="/san-pham/den-ban-an">Đèn bàn ăn</a>
+                              </li>
+                              <li>
+                                <a href="/san-pham/ghe-van-phong">
+                                  Ghế văn phòng
+                                </a>
+                              </li>
+                              <li>
+                                <a href="/san-pham/den-trang-tri">
+                                  Đèn trang trí
+                                </a>
+                              </li>
+                              <li>
+                                <a href="/san-pham/ke-sach">Kệ sách</a>
+                              </li>
                             </ul>
                           </div>
                           <div className="sub-menu-products">
@@ -166,31 +238,41 @@ const Header = () => {
                               <div className="product-item">
                                 <a href="/san-pham/tu-trang-tri">
                                   <div className="product-image"></div>
-                                  <div className="product-title">Tủ trang trí</div>
+                                  <div className="product-title">
+                                    Tủ trang trí
+                                  </div>
                                 </a>
                               </div>
                               <div className="product-item">
                                 <a href="/san-pham/den-phong-khach">
                                   <div className="product-image"></div>
-                                  <div className="product-title">Đèn phòng khách</div>
+                                  <div className="product-title">
+                                    Đèn phòng khách
+                                  </div>
                                 </a>
                               </div>
                               <div className="product-item">
                                 <a href="/san-pham/giuong-ngu">
                                   <div className="product-image"></div>
-                                  <div className="product-title">Giường ngủ</div>
+                                  <div className="product-title">
+                                    Giường ngủ
+                                  </div>
                                 </a>
                               </div>
                               <div className="product-item">
                                 <a href="/san-pham/tu-quan-ao">
                                   <div className="product-image"></div>
-                                  <div className="product-title">Tủ quần áo</div>
+                                  <div className="product-title">
+                                    Tủ quần áo
+                                  </div>
                                 </a>
                               </div>
                               <div className="product-item">
                                 <a href="/san-pham/ban-trang-diem">
                                   <div className="product-image"></div>
-                                  <div className="product-title">Bàn trang điểm</div>
+                                  <div className="product-title">
+                                    Bàn trang điểm
+                                  </div>
                                 </a>
                               </div>
                             </div>
@@ -228,7 +310,10 @@ const Header = () => {
                       <div className="search-input-wrapper">
                         <input type="text" placeholder="Tìm kiếm sản phẩm" />
                         <button type="submit">
-                          <img src="../public/images/icons/magnifier-gray.svg" alt="search" />
+                          <img
+                            src="../public/images/icons/magnifier-gray.svg"
+                            alt="search"
+                          />
                         </button>
                       </div>
                       <div className="search-content d-flex">
@@ -237,37 +322,55 @@ const Header = () => {
                           <div className="search-links">
                             <div className="search-link-item">
                               <a href="/search?q=sofa+phòng+khách">
-                                <img src="../public/images/icons/magnifier-gray.svg" alt="search" />
+                                <img
+                                  src="../public/images/icons/magnifier-gray.svg"
+                                  alt="search"
+                                />
                                 <span>sofa phòng khách</span>
                               </a>
                             </div>
                             <div className="search-link-item">
                               <a href="/search?q=giường+ngủ+gỗ+tự+nhiên">
-                                <img src="../public/images/icons/magnifier-gray.svg" alt="search" />
+                                <img
+                                  src="../public/images/icons/magnifier-gray.svg"
+                                  alt="search"
+                                />
                                 <span>giường ngủ gỗ tự nhiên</span>
                               </a>
                             </div>
                             <div className="search-link-item">
                               <a href="/search?q=bàn+ăn+4+ghế">
-                                <img src="../public/images/icons/magnifier-gray.svg" alt="search" />
+                                <img
+                                  src="../public/images/icons/magnifier-gray.svg"
+                                  alt="search"
+                                />
                                 <span>bàn ăn 4 ghế</span>
                               </a>
                             </div>
                             <div className="search-link-item">
                               <a href="/search?q=bàn+làm+việc+tại+nhà">
-                                <img src="../public/images/icons/magnifier-gray.svg" alt="search" />
+                                <img
+                                  src="../public/images/icons/magnifier-gray.svg"
+                                  alt="search"
+                                />
                                 <span>bàn làm việc tại nhà</span>
                               </a>
                             </div>
                             <div className="search-link-item">
                               <a href="/search?q=kệ+tivi+treo+tường">
-                                <img src="../public/images/icons/magnifier-gray.svg" alt="search" />
+                                <img
+                                  src="../public/images/icons/magnifier-gray.svg"
+                                  alt="search"
+                                />
                                 <span>kệ tivi treo tường</span>
                               </a>
                             </div>
                             <div className="search-link-item">
                               <a href="/search?q=thảm+trải+sàn+phòng+khách">
-                                <img src="../public/images/icons/magnifier-gray.svg" alt="search" />
+                                <img
+                                  src="../public/images/icons/magnifier-gray.svg"
+                                  alt="search"
+                                />
                                 <span>thảm trải sàn phòng khách</span>
                               </a>
                             </div>
@@ -310,13 +413,17 @@ const Header = () => {
                             <div className="product-item">
                               <a href="/san-pham/tu-trang-tri">
                                 <div className="product-image"></div>
-                                <div className="product-title">Tủ trang trí</div>
+                                <div className="product-title">
+                                  Tủ trang trí
+                                </div>
                               </a>
                             </div>
                             <div className="product-item">
                               <a href="/san-pham/den-phong-khach">
                                 <div className="product-image"></div>
-                                <div className="product-title">Đèn phòng khách</div>
+                                <div className="product-title">
+                                  Đèn phòng khách
+                                </div>
                               </a>
                             </div>
                             <div className="product-item">
@@ -334,7 +441,9 @@ const Header = () => {
                             <div className="product-item">
                               <a href="/san-pham/ban-trang-diem">
                                 <div className="product-image"></div>
-                                <div className="product-title">Bàn trang điểm</div>
+                                <div className="product-title">
+                                  Bàn trang điểm
+                                </div>
                               </a>
                             </div>
                           </div>
@@ -350,7 +459,7 @@ const Header = () => {
                   <img src="/images/icons/wishlist.svg" alt="wishlist" />
                 </button>
               </div>
-              <span>|</span>
+              <span className="header-actions-separator"></span>
               <div className="header-actions-buttons">
                 <a href="/dang-nhap" className="btn btn-outline">
                   Đăng Nhập
@@ -358,6 +467,11 @@ const Header = () => {
                 <a href="/dang-ky" className="btn btn-primary">
                   Đăng Ký
                 </a>
+              </div>
+              <div className="header-hamburger">
+                <button className="btn-icon hamburger-btn">
+                  <img src="/images/icons/Hamburger_MD.svg" alt="hamburger" />
+                </button>
               </div>
             </div>
           </div>
