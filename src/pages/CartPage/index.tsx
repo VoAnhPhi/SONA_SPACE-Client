84e6d4af598d12c78ee4a7b6cffc7749b4eae61a
ToTrongNhan1209 <totrongnhan1209.com>
2025-06-30 02:46:34 +0700

    Update | Toast cart | TrongNhan

--- src/pages/CartPage/index.tsx
+++ src/pages/CartPage/index.tsx
@@ -5,6 +5,7 @@ import Footer from "../../components/Footer";
 import GetInTouch from "../../components/GetInTouch";
 import PolicyProduct from "../../components/Policy";
 import { useAuth } from "../../contexts/AuthContext";
+import { ToastContainer, toast } from "react-toastify";
 import { loadCartService, updateCartQuantityService, removeFromCartService, clearCartService } from "../../services/cartService";
 
 interface CartItemProps {
@@ -103,7 +104,8 @@ const recalculateSummary = (items: CartItemProps[]) => {
       await removeFromCartService(id);
       const updatedItems = cartItems.filter((item) => item.id !== id);
       setCartItems(updatedItems);
-      recalculateSummary(updatedItems); // ← cập nhật lại tổng tiền
+      recalculateSummary(updatedItems); 
+          toast.success("Đã xóa sản phẩm khỏi giỏ hàng!");
     } catch (error) {
       console.error("Lỗi khi xóa sản phẩm:", error);
     }
@@ -117,8 +119,10 @@ const recalculateSummary = (items: CartItemProps[]) => {
       const result = await clearCartService();
       if (result.success !== false) {
         await loadWishlist();
+         toast.success("Đã xóa toàn bộ giỏ hàng!");
       } else {
         console.log(result.message);
+          toast.error(result.message || "Không thể xóa giỏ hàng.");
       }
     } catch (error) {
       alert("Lỗi khi xóa giỏ hàng.");
