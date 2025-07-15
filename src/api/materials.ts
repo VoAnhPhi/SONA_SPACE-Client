b4190712435e06798143a0c5f051f0a44230e204
ThanhTu260104 <tuttps40130@gmail.com>
2025-07-15 18:27:54 +0700

    Update | Product Detail | Thanh Tu

--- src/api/materials.ts
+++ src/api/materials.ts
@@ -3,22 +3,20 @@ import type { Attribute } from "../types";
 
 const API_URL = "http://localhost:3501/api";
 
-export const getAllAttributes = async (): Promise<Attribute[]> => {
+export const getAllMaterials = async (): Promise<Attribute[]> => {
   try {
     const response = await axios.get(`${API_URL}/materials`);
-    if (!response.data) throw new Error("No data received for attributes");
+    if (!response.data) throw new Error("No data received for materials");
     return response.data;
   } catch (error) {
-    console.error("Error fetching attributes:", error);
+    console.error("Error fetching materials:", error);
     throw error;
   }
 };
-export const getAllAttributesSlugs = async (
-  slug: string
-): Promise<string[]> => {
+export const getAllMaterialsSlugs = async (slug: string): Promise<string[]> => {
   try {
     const response = await axios.get(`${API_URL}/materials/${slug}`);
-    if (!response.data) throw new Error("No data received for attribute slugs");
+    if (!response.data) throw new Error("No data received for material slugs");
     return response.data;
   } catch (error) {
     console.error(`Error fetching material for slugs ${slug}:`, error);
