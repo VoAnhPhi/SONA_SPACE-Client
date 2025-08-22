7a75f354aa054578e9a0780953da46df38ff6141
VoAnhPhi <vophi120705@gmail.com>
2025-08-22 08:11:37 +0700

    Build | chat bot | voanhphi

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-import PopupAd from "./components/PopupAd/index.tsx";
 import ChatBot from "./components/ChatBot/index.tsx";
+import PopupAd from "./components/PopupAd/index.tsx";
 import App from "./App.tsx";
 import 'antd/dist/reset.css';
 
 createRoot(document.getElementById("root")!).render(
   <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
     <StrictMode>
+      <ChatBot />
       <App />
       <PopupAd />
-      <ChatBot />
     </StrictMode>
   </GoogleOAuthProvider>
 );
